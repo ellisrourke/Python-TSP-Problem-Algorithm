5450b5de73f895a3706dc16aca55a3970a99284e
ellisrourkePC <ellisrourke@gmail.com>
2019-07-31 18:41:15 +1000

    progress

--- TSP.py
+++ TSP.py
@@ -3,12 +3,13 @@ import math
 import random
 import networkx as nx
 import matplotlib.pyplot as plt
+import numpy
 
 class tspSolver():
     def __init__(self):
         self.problem = tsplib.load_problem("in_data/eil51.tsp", special="euclidean_2d_jitter")
         #self.tour = list(range(1, self.problem.dimension + 1))
-        self.tour = list(range(1,15))
+        self.tour = list(range(1,20))
         self.shortestPathLength = 1000000
 
     def findPathLength(self):
@@ -18,15 +19,40 @@ class tspSolver():
         for i in range(0, len(self.tour)):
             xList.append(self.problem.get_display(self.tour[i])[0])
             yList.append(self.problem.get_display(self.tour[i])[1])
-
         #for i in range(self.problem.dimension-1):
-        for i in range(13):
+        for i in range(18):
             pathDistance += tsplib.distances.euclidean((xList[i], yList[i]), (xList[i + 1], yList[i + 1]))
-
         return pathDistance
 
+
+    '''
+    def nearestNeighbour(self):
+        for i in range(0, len(self.tour)):
+            xList.append(self.problem.get_display(self.tour[i])[0])
+            yList.append(self.problem.get_display(self.tour[i])[1])
+
+        for i in range(0,len(x)):
+            minDist = 9999
+            keep = [0,0]
+            for j in range(i,len(x)):
+                keep = [0, 0]
+                dist = tsplib.distances.euclidean((x[i],y[i]),(x[j],y[j]))
+                if dist < minDist:
+                    minDist = dist
+                    if x[j] not in pathx and y[j] not in pathy:
+                        minDist = dist
+                        keep[0] = x[j]
+                        keep[1] = y[j]
+            pathx.append(keep[0])
+            pathy.append(keep[1])
+        plt.plot(pathx, pathy)
+        plt.scatter(pathx, pathy)
+        plt.show()
+
+
+    '''
     def findShortestRandom(self):
-        for i in range(2000000):
+        for i in range(9999999):
             random.shuffle(self.tour)
             xList = []
             yList = []
@@ -42,39 +68,45 @@ class tspSolver():
                 plt.scatter(xList, yList)
                 plt.show()
 
-'''
-    def nearestNeighbour(self):
+
+
+    def nn(self):
         xList = []
         yList = []
+        distances = [0 for i in range(len(self.tour))]
+
+        newX = []
+        newY = []
+
         for i in range(0, len(self.tour)):
             xList.append(self.problem.get_display(self.tour[i])[0])
             yList.append(self.problem.get_display(self.tour[i])[1])
-        #for i in range(len(self.tour)):
-        #    print(xList[i],"|",yList[i])
+
+        newX.append(xList[0])
+        newY.append(yList[0])
+        print(xList,yList)
+        xList.pop(0)
+        yList.pop(0)
+
+        for i in range(len(xList)):
+            distances = []
+            for j in range(len(xList)):
+                '''find distances to each node'''
+                distances.append((tsplib.distances.euclidean((newX[-1],newY[-1]),(xList[j],yList[j]))))
+            lowest = distances.index(min(distances))
+
+            newX.append(xList[lowest])
+            newY.append(yList[lowest])
+            xList.pop(lowest)
+            yList.pop(lowest)
+            print(distances)
+
+        print(newX,newY)
 
 
-        x = []
-        y = []
-
-        x.append(self.problem.get_display(self.tour[0])[0])
-        y.append(self.problem.get_display(self.tour[0])[1])
-        dist = 10000000
-        for i in range(len(self.tour)):
-            j = i
-            for j in range(len(self.tour)):
-                if(math.sqrt(self.problem.get_display(self.tour[i][0])))
-                
-        #sqrt((x2-x1)^2+(y2-y1)^2)
-        plt.plot(x,y)
-        plt.scatter(x,y)
+        plt.plot(newX,newY)
+        plt.scatter(newX,newY)
         plt.show()
-
-'''
-
-
-
-
-
 
 
 
@@ -87,5 +119,6 @@ tour = list(range(1,6))
 
 '''
 x = tspSolver()
-x.findShortestRandom()
+#x.findShortestRandom()
 #x.nearestNeighbour()
+x.nn()
